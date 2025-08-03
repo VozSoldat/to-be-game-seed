4e5c0588add3aa7afef7cdd34d364b1d8548d73b
Daffa Maulana Satria <daffamaulanasatria@gmail.com>
2025-08-03 21:12:06 +0700

    feat finish button (not implemented the logic)

---
+++ Assets/App/Scripts/Components/Finishbutton/FinishButtonLogic.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FinishButtonLogic : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
