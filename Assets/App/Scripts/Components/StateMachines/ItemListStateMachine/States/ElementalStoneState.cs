ccb5a7b7be433b6cafb30801926e89c21cfce93a
VozSoldat <muhamaderril7@gmail.com>
2025-08-03 08:52:52 +0700

    !chore: implement state-based category button sprite swapping and TRASH button to return to glass selection
    <<Breaking Change>>
    
    Update item list state machine and states to accommodate category buttons sprite state.
    
    Update TRASH button and state context to make the TRASH button repopulate the item list with GLASS and transition to the correct GLASS state.

--- Assets/App/Scripts/Components/StateMachines/ItemListStateMachine/States/ElementalStoneState.cs
+++ Assets/App/Scripts/Components/StateMachines/ItemListStateMachine/States/ElementalStoneState.cs
@@ -5,5 +5,16 @@ namespace StateMachines.ItemListStateMachine
     public class ElementalStoneState : ItemListState
     {
         public ElementalStoneState() : base(ItemCategory.ElementalStone) { }
+
+        public override void Enter(MB_ItemListStateContext context)
+        {
+            base.Enter(context);
+            SetButtonState(context, context.elementalStoneButtonPlace, true);
+        }
+        public override void Exit(MB_ItemListStateContext context)
+        {
+            base.Exit(context);
+            SetButtonState(context, context.elementalStoneButtonPlace, false);
+        }
     }
 }
