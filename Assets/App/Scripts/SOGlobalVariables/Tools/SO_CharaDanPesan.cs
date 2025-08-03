afa693e27ad0111c278d85b588e8c8d7d7739821
VozSoldat <muhamaderril7@gmail.com>
2025-08-03 17:02:31 +0700

    HOTFIX: update CharaDanPesan

--- Assets/App/Scripts/SOGlobalVariables/Tools/SO_CharaDanPesan.cs
+++ Assets/App/Scripts/SOGlobalVariables/Tools/SO_CharaDanPesan.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SO_CharaDanPesan", menuName = "Scriptable Objects/SO_CharaDanPesan")]
 public class SO_CharaDanPesan : ScriptableObject
 {
-    public string characterName;
-    public Sprite staticSprite;
-    public Animation idleAnimation;
-    public Animation drinkAnimation;
+    public SO_Character character;
     public ItemData characterOrder;
 }
